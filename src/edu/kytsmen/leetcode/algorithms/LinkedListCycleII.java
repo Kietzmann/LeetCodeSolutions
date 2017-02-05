d21f364e76b045f1c9bc255a1451c99f50a8f684
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-02-05 03:00:09 +0200

    Added another 2 solutions

---
+++ src/edu/kytsmen/leetcode/algorithms/LinkedListCycleII.java
@@ -0,0 +1,27 @@
+package edu.kytsmen.leetcode.algorithms;
+
+/**
+ * Created by dmytro on 05.02.17.
+ */
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                slow = head;
+                while (slow != fast) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
