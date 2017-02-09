2ca39dd48fce94ee34f592ffdf7c3ba10e021ab8
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-02-09 13:10:37 +0200

    Added solutions to another 2 problems

---
+++ src/edu/kytsmen/leetcode/algorithms/IsIsomorphic.java
@@ -0,0 +1,26 @@
+package edu.kytsmen.leetcode.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by dkytsmen on 2/9/17.
+ */
+public class IsIsomorphic {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        for (int i = 0; i < sArray.length; i++) {
+            if (map.containsKey(sArray[i])) {
+                if (map.get(sArray[i]) != tArray[i])// if not consistant with previous ones
+                    return false;
+            } else {
+                if (map.containsValue(tArray[i])) //if c2 is already being mapped
+                    return false;
+                map.put(sArray[i], tArray[i]);
+            }
+        }
+        return true;
+    }
+}
---
+++ src/edu/kytsmen/leetcode/algorithms/LongestPalindrome.java
@@ -0,0 +1,33 @@
+package edu.kytsmen.leetcode.algorithms;
+
+import java.util.Arrays;
+
+/**
+ * Created by dkytsmen on 2/9/17.
+ */
+public class LongestPalindrome {
+    public int longestPalindrome(String s) {
+        char[] sArray = s.toCharArray();
+        int[] entries = new int[256];
+        Arrays.fill(entries, 0);
+        for (int i = 0; i < sArray.length; i++) {
+            entries[sArray[i]]++;
+        }
+
+        int count = 0;
+        int odd = 0;
+        for (int i = 0; i < entries.length; i++) {
+            if ((entries[i] & 1) == 1) {
+                if (odd == 0) {
+                    odd += entries[i];
+                } else {
+                    odd += entries[i] - 1;
+                }
+            } else if ((entries[i] & 1) == 0 && entries[i] != 0) {
+                count += entries[i];
+            }
+        }
+
+        return count + odd;
+    }
+}
