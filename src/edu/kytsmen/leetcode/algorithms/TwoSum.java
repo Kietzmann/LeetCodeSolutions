c60e22819eea7dcc823bc4f8299316684314ddbd
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-11-23 20:14:23 +0200

    Another solution

---
+++ src/edu/kytsmen/leetcode/algorithms/TwoSum.java
@@ -0,0 +1,22 @@
+package edu.kytsmen.leetcode.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return new int[]{0, 0};
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            } else {
+                map.put(target - nums[i], i);
+            }
+        }
+        return new int[]{0, 0};
+    }
+}
